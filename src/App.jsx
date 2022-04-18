726a5b7f422de3cff15447cfc5e2f0b1220ea9a3
Jonathon Béasse <crypticspaghetti@protonmail.com>
2022-04-18 16:48:49 -0600

    Center text

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import SearchBox from "./SearchBox";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="tc">
         <h1>RoboFriends</h1>
         <SearchBox />
         <CardList listItems={robots} />
