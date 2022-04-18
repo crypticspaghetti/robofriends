44a61855c0e8dd8c0e92811f3300a5759c980938
Jonathon Béasse <crypticspaghetti@protonmail.com>
2022-04-18 17:00:37 -0600

    Add fuzzy search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,29 @@ import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots,
+      searchField: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList listItems={robots} />
+        <SearchBox onChange={this.onSearchChange} />
+        <CardList listItems={filteredRobots} />
       </div>
     );
   }
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,10 +1,11 @@
-const SearchBox = () => {
+const SearchBox = ({ onChange }) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="Search robots"
+        onChange={onChange}
       />
     </div>
   );
