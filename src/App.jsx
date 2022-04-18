f6630e193c1bb3ec381b1b5718ed4828afe92a6f
Jonathon Béasse <crypticspaghetti@protonmail.com>
2022-04-18 16:42:22 -0600

    Add title

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { Component } from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <h1>RoboFriends</h1>
+        <CardList listItems={robots} />
+      </div>
+    );
   }
 }
 
