a7bd8d01fb8ac960b52d5d90916f1d991fec7ffa
Jonathon Béasse <crypticspaghetti@protonmail.com>
2022-04-18 16:48:25 -0600

    Add SearchBox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
+import SearchBox from "./SearchBox";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>RoboFriends</h1>
+        <SearchBox />
         <CardList listItems={robots} />
       </div>
     );
